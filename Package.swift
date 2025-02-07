161de2f38f4524ab3c9dd0820c39eee9fc1c6667
abbayram <94920176+abbayram@users.noreply.github.com>
2025-02-07 22:17:55 +0100

    Fix import errors for GoogleSignIn and PackageDescription
    Add GoogleSignIn dependency and import statement.
    
    * Modify `Package.swift` to include `GoogleSignIn` dependency in the `dependencies` array.
    * Add `GoogleSignIn` product to the `dependencies` array of the `coffemonkey` target.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/abbayram/coffemonkey?shareId=XXXX-XXXX-XXXX-XXXX).

--- Package.swift
+++ Package.swift
@@ -11,13 +11,15 @@ let package = Package(
             targets: ["coffemonkey"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "8.0.0")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "8.0.0"),
+        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "6.0.0")
     ],
     targets: [
         .target(
             name: "coffemonkey",
             dependencies: [
                 .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
                 "LogoAnimationView"
             ]),
         .testTarget(
